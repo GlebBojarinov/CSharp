4d25cc55c66b4be2a0ce1a413c7b22400c31242b
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-03-07 20:04:17 +0100

    add lesson1:Task4

---
+++ lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 35;
+int b = 15;
+int c = 44;
+int d = 41;
+int e = 19;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
