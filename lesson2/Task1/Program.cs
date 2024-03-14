7ad7dd8d0723be7293675bbc53dcb3ffafef2939
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-03-14 12:17:28 +0100

    add Lesson2:Task1

---
+++ lesson2/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 1;
+arr[4] = 9;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+//System.Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5](5, 8, 3, 7, 4);
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+//python arr = [1,2,3,4,5]
