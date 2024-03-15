443577f1a193ac6969b98582ee35ac9c0b4825dd
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-03-15 11:31:10 +0100

    lesson2:Task3

---
+++ lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 11, 57, 98, 33, 66, 77, 88, 44, 7, 8 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
