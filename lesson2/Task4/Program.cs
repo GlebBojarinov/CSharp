7a136b1fcf60047819c79350168da595dc13ef64
Gleb Bojarinov <glebbojarinov@yahoo.com>
2024-03-15 12:43:07 +0100

    add lesson2:Task4

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int[] array = { 3, 1, 6, 4, 9 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+
+System.Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
